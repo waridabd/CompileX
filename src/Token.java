9331ac717017ae48119189c9d26b6cf2393e9b2e
waridabd <waridabd20@gmail.com>
2026-04-26 21:09:04 +0600

    Add TokenType enum and Token class for Bangla lexer

---
+++ src/Token.java
@@ -0,0 +1,17 @@
+
+
+public class Token {
+    public final TokenType type;    
+    public final String    lexeme;  
+    public final int       line;    
+    public Token(TokenType type, String lexeme, int line) {
+        this.type   = type;
+        this.lexeme = lexeme;
+        this.line   = line;
+    }
+    @Override
+    public String toString() {
+        return String.format("Token{ type=%-18s  lexeme=%-15s  line=%d }",
+                             type, "\"" + lexeme + "\"", line);
+    }
+}
---
+++ src/TokenType.java
@@ -0,0 +1,18 @@
+
+
+public enum TokenType {
+    TYPE_SHONGKHA,   
+    TYPE_BAKKO,      
+    NUMBER,        
+    STRING,          
+    IDENTIFIER,     
+    ASSIGN,          
+    PLUS,            
+    MINUS,           
+    MULTIPLY,        
+    DIVIDE,          
+    SEMICOLON,       
+    LPAREN,          
+    RPAREN,          
+    EOF              
+}
