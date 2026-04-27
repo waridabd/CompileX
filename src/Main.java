b6839624c09d198a9a16238612ead07fd5a95568
anikabushra111 <anikabushra467@gmail.com>
2026-04-27 20:16:31 +0600

    Integrate semantic analysis calls in Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,29 @@ public class Main {
                 "বাক্য নাম = \"মানুষ\";\n" +
                 "মোট = মোট + ১;\n";
 
-        String invalidProgram =
+        String syntaxErrorProgram =
                 "সংখ্যা বয়স = ৫\n" +
                 "সংখ্যা মোট = বয়স + ;\n" +
                 "বাক্য নাম = \"মানুষ\";\n" +
                 "মোট = (১ + ২;\n";
 
+        String undeclaredProgram =
+                "সংখ্যা বয়স = ৫;\n" +
+                "মোট = বয়স + ২;\n";
+
+        String typeMismatchProgram =
+                "সংখ্যা বয়স = \"মানুষ\";\n" +
+                "বাক্য নাম = \"রাফি\";\n";
+
+        String duplicateProgram =
+                "সংখ্যা বয়স = ৫;\n" +
+                "সংখ্যা বয়স = ১০;\n";
+
         runTest("সঠিক প্রোগ্রাম", validProgram);
-        runTest("ভুল প্রোগ্রাম", invalidProgram);
+        runTest("সিনট্যাক্স ভুল প্রোগ্রাম", syntaxErrorProgram);
+        runTest("Undeclared variable test", undeclaredProgram);
+        runTest("Type mismatch test", typeMismatchProgram);
+        runTest("Duplicate declaration test", duplicateProgram);
     }
 
     private static void runTest(String title, String source) {
@@ -26,7 +41,6 @@ public class Main {
 
         Lexer lexer = new Lexer(source);
         List<Token> tokens = lexer.tokenize();
-
         lexer.printTokens();
 
         if (lexer.hasErrors()) {
@@ -36,5 +50,13 @@ public class Main {
 
         Parser parser = new Parser(tokens);
         parser.parseProgram();
+
+        if (parser.hasErrors()) {
+            System.out.println("সিনট্যাক্স ত্রুটি থাকায় সেমান্টিক বিশ্লেষণ শুরু হয়নি।");
+            return;
+        }
+
+        SemanticAnalyzer semanticAnalyzer = new SemanticAnalyzer(tokens);
+        semanticAnalyzer.analyze();
     }
 }
