2c1df727f8ec9a17bd4509052695603b751ccade
waridabd <waridabd20@gmail.com>
2026-05-20 20:20:42 +0600

    add value field to support ASCII digit output for code generation

--- src/Token.java
+++ src/Token.java
@@ -1,16 +1,29 @@
 public class Token {
-    public final TokenType type;    
-    public final String    lexeme;  
-    public final int       line;    
-    public Token(TokenType type, String lexeme, int line) {
+    public final TokenType type;
+    public final String lexeme;
+    public final String value;
+    public final int line;
+
+    public Token(TokenType type, String lexeme, String value, int line) {
         this.type   = type;
         this.lexeme = lexeme;
+        this.value  = value;
         this.line   = line;
     }
-    
+
+    public Token(TokenType type, String lexeme, int line) {
+        this(type, lexeme, lexeme, line);
+    }
+
     @Override
     public String toString() {
-        return String.format("Token{ type=%-18s  lexeme=%-15s  line=%d }",
-                             type, "\"" + lexeme + "\"", line);
+        if (type == TokenType.NUMBER && !lexeme.equals(value)) {
+            return String.format(
+                "Token{ type=%-18s  lexeme=%-15s  value=%-10s  line=%d }",
+                type, "\"" + lexeme + "\"", "\"" + value + "\"", line);
+        }
+        return String.format(
+            "Token{ type=%-18s  lexeme=%-15s  line=%d }",
+            type, "\"" + lexeme + "\"", line);
     }
 }
